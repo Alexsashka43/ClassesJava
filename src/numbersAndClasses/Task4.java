ed4ac87a0b7f27561bc1e5ecacd0da3e2627fbb3
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2021-09-15 15:46:51 +0300

    HW2

--- src/numbersAndClasses/Task4.java
+++ src/numbersAndClasses/Task4.java
@@ -2,10 +2,8 @@ package numbersAndClasses;
 
 public class Task4 {
     public static void main(String[] args) {
-        Vector vector1 = new Vector(2, 2, 2);
-        Vector vector2 = new Vector(2, 2, 2);
-        System.out.println(vector1.findVectorLength());
-        System.out.println(vector1.scalarProduct(vectorArray));
+        Vector v1 = new Vector(2, 2, 2);
+        Vector v2 = new Vector(3, 3, 3);
     }
 }
 
@@ -18,23 +16,48 @@ class Vector {
         this.x = x;
         this.y = y;
         this.z = z;
-        double [] vectorArray = {x, y, z};
-
     }
-
-//    double[] vectorArray() {
-//
-//    }
-
 
     double findVectorLength() {
-        double vectorLength = Math.sqrt(x * x + y * y + z * z);
-        return vectorLength;
+        return Math.sqrt(x * x + y * y + z * z);
     }
 
-    double[] scalarProduct(double [] a, double [] b) {
-        double [] c = new double[3];
-        for (int i = 0; i < a.length; i++) {
-            c[i] = a[i] * b[i];
+    double scalarVectors(Vector v) {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    numbersAndClasses.Vector multiplicationVectors(Vector v){ //псевдовектор
+        return new Vector(
+                y * v.z - z * v.y,
+                z * v.x - x * v.z,
+                x * v.y - y * v.x);
+    }
+
+    double cosVector(Vector v){
+        return scalarVectors(v)/(findVectorLength()*v.findVectorLength());
+    }
+
+    public Vector add(Vector v) {
+        return new Vector(
+                x + v.x,
+                y + v.y,
+                z + v.z
+        );
+    }
+
+    public Vector subtract(Vector v) {
+        return new Vector(
+                x - v.x,
+                y - v.y,
+                z - v.z
+        );
+    }
+
+    public static Vector[] randomVector (int n){
+        Vector[] vectors = new Vector[n];
+        for(int i =0; i < n; i++){
+            vectors[i] = new Vector(Math.random(), Math.random(), Math.random());
         }
-        return c;
+        return vectors;
+    }
+}
